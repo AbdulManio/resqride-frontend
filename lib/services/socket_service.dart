35b3bb946615f3360db55699b57e56dbb48bd53d
abdul manio <manio.shaikh04@gmail.com>
2026-07-18 00:27:13 +0500

    Update URLs to Vercel, fix socket reconnection

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -14,6 +14,7 @@ class SocketService {
     _socket = IO.io(_serverUrl, <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
+      'reconnection': false,
     });
 
     _socket!.connect();
